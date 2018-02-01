0b1c3668bd7cccab32f34e87ad5b8e2f788f0130
huzongtao <huzongtao@rtmap.com>
2018-02-01 16:15:31 +0800

    fix bug

--- config-client/src/main/java/com/rtmap/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/rtmap/configclient/ConfigClientApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author huzongtao
+ */
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
